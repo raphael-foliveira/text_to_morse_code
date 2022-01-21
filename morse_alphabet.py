07d01007d97257100575778e8c4b5f34e4ff7b0e
Raphael Oliveira <raphael.foliveira1@gmail.com>
2022-01-21 13:45:46 -0300

    add decoding function

--- morse_alphabet.py
+++ morse_alphabet.py
@@ -5,3 +5,4 @@ morse_characters = ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. " \
                    "--- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --.. " \
                    ".---- ..--- ...-- ....- ..... -.... --... ---.. ----. ----- / ".split(" ")
 morse_dict = {letter: code for (letter, code) in zip(alphabet, morse_characters)}
+decode_dict = {code: letter for (letter, code) in zip(alphabet, morse_characters)}
--- morse_converter.py
+++ morse_converter.py
@@ -1,9 +1,15 @@
-from morse_alphabet import morse_dict
+from morse_alphabet import morse_dict, decode_dict
 
 
 def convert_text_to_morse(text: str):
     converted_text = ""
-    converted_text_list = [f"{morse_dict[character]} " for character in text.lower() if character in morse_dict]
-    for code in converted_text_list:
-        converted_text += code
+    if "-" not in text and "." not in text:
+        converted_text_list = [f"{morse_dict[character]} " for character in text.lower() if character in morse_dict]
+        for code in converted_text_list:
+            converted_text += code
+    else:
+        code_list = text.split(" ")
+        for code in code_list:
+            if len(code) > 0:
+                converted_text += decode_dict[code]
     return converted_text
